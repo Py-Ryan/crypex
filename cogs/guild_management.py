27607c91533fc8bcd7f8fa3f0a80b5b6a72927a1
Py-Ryan <63752837+Py-Ryan@users.noreply.github.com>
2020-04-24 04:11:25 -0400

    Argument changes, logic improvement.
    guild_management:
        - Various rest arguments such as 'reason' have been converted to strings, rather than lists.
        - create_channel category_name is now reassigned during the try: except, rather than after.

--- cogs/guild_management.py
+++ cogs/guild_management.py
@@ -1,7 +1,6 @@
 import discord
-import datetime
 from discord.ext import commands
-from typing import Optional, List, Union
+from typing import Optional, Union, Deque
 
 
 class GuildManagement(commands.Cog):
@@ -28,49 +27,44 @@ class GuildManagement(commands.Cog):
                 topic: (str)
                     The topic of the channel.
         """
+
+        def _check_(message) -> bool:
+            return message.author == ctx.author
+
+        await ctx.send(embed=self.client.templates.base_embed('What should the channel\'s name be?'))
+        channel_name: str = await self.client.wait_for('message', check=_check_)
+
+        await ctx.send(embed=self.client.templates.base_embed('Should the channel be a VC or Text? Pick one.'))
+        channel_type: str = await self.client.wait_for('message', check=_check_)
+
+        await ctx.send(embed=self.client.templates.base_embed(
+            'If you\'d like the channel to be added to a category, enter that categories exact name. Else say no.'))
+        category_name: str = await self.client.wait_for('message', check=_check_)
+
         try:
-            def _check_(message) -> bool:
-                return message.author == ctx.author
+            category_name: Optional[discord.CategoryChannel] = \
+                await commands.CategoryChannelConverter().convert(ctx, category_name.content)
+        except commands.BadArgument:  # In case there is no category matching the argument.
+            category_name = None
+            pass
 
-            await ctx.send(embed=self.client.templates.base_embed('What should the channel\'s name be?'))
-            channel_name: str = await self.client.wait_for('message', check=_check_)
+        await ctx.send(embed=self.client.templates.base_embed('Enter a channel topic.'))
+        topic: str = await self.client.wait_for('message', check=_check_)
 
-            await ctx.send(embed=self.client.templates.base_embed('Should the channel be a VC or Text? Pick one.'))
-            channel_type: str = await self.client.wait_for('message', check=_check_)
-
-            await ctx.send(embed=self.client.templates.base_embed(
-                'If you\'d like the channel to be added to a category, enter that categories exact name. Else say no.'))
-            category_name: str = await self.client.wait_for('message', check=_check_)
-
-            try:
-                category_name: Optional[discord.CategoryChannel] = \
-                    await commands.CategoryChannelConverter().convert(ctx, category_name.content)
-            except commands.BadArgument:    # In case there is no category matching the argument.
-                pass
-
-            #   Thanks d.py. commands.CategoryChannelConverter().convert(...) returns an exception, rather than a falsy.
-            if not isinstance(category_name, discord.CategoryChannel):
-                category_name = None
-
-            await ctx.send(embed=self.client.templates.base_embed('Enter a channel topic.'))
-            topic: str = await self.client.wait_for('message', check=_check_)
-
-            if channel_name and channel_type.content.lower() in {'vc', 'voice', 'voicechannel', 'voice_channel'}:
-                await ctx.guild.create_voice_channel(name=channel_name.content,
-                                                     category=category_name)
-            elif channel_name and channel_type and topic:
-                await ctx.guild.create_text_channel(name=channel_name.content,
-                                                    category=category_name,
-                                                    topic=topic.content)
-        except Exception as e:
-            raise Exception('Oops. I can\'t create a channel with the information you gave, sorry!')
+        if channel_name and channel_type.content.lower() in {'vc', 'voice', 'voicechannel', 'voice_channel'}:
+            await ctx.guild.create_voice_channel(name=channel_name.content,
+                                                 category=category_name)
+        elif channel_name and channel_type and topic:
+            await ctx.guild.create_text_channel(name=channel_name.content,
+                                                category=category_name,
+                                                topic=topic.content)
 
     @commands.command()
     @commands.guild_only()
     @commands.bot_has_permissions(manage_channels=True)
     @commands.has_permissions(manage_channels=True)
     async def delete_channel(self, ctx: commands.Context, channel: Union[discord.TextChannel, discord.VoiceChannel],
-                             *reason) -> None:
+                             *, reason: str = 'None Provided.') -> None:
         """Delete a channel.
 
             Parameters
@@ -80,10 +74,8 @@ class GuildManagement(commands.Cog):
             reason: (Optional[str])
                 The reason for deleting the channel.
         """
-        if not reason:
-            reason = ['None']
         try:
-            await channel.delete(reason=' '.join(reason))
+            await channel.delete(reason=reason)
             await ctx.send(embed=self.client.templates.base_embed(f'Goodbye, {str(channel)}!'))
         except Exception:
             raise Exception(f'There is no channel named {str(channel)}')
@@ -92,38 +84,34 @@ class GuildManagement(commands.Cog):
     @commands.guild_only()
     @commands.bot_has_permissions(ban_members=True)
     @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx, _user: Union[discord.Member, discord.User, int], *reason) -> None:
+    async def ban(self, ctx, puser: Union[discord.Member, discord.User, int], *, reason: str = 'None Provided') -> None:
         """Ban a guild member.
 
-            member: (discord.Member)
+            user: (Union[discord.Member, discord.User, int])
                 The member to ban.
             reason: (Optional[str])
                 The reason for the ban.
         """
-        user = _user
+        user = puser
         if type(user) is int:
             try:
-                user = discord.Object(id=_user)
+                user = discord.Object(id=puser)
             except Exception:
                 raise Exception(f'There is no user with the id {user}')
-
-        # Prevent 'reason' from appearing as an empty tuple.
-        if not reason:
-            reason = ['None']
 
         if user in ctx.guild.members:
             embed = self.client.templates.base_embed()
             embed.title = f'You have been banned from {ctx.guild.name}.'
             embed.set_thumbnail(url=ctx.guild.icon_url)
             embed.add_field(name='Banned By:', value=f'{ctx.author}\n({ctx.author.id})', inline=True)
-            embed.add_field(name='Ban Reason:', value=' '.join(reason), inline=True)
+            embed.add_field(name='Ban Reason:', value=reason, inline=True)
             embed.set_footer(icon_url=user.avatar_url, text='Sorry, man. - Crypex Bot')
             await user.send(embed=embed)
 
-        await ctx.guild.ban(user, reason=' '.join(reason))
+        await ctx.guild.ban(user, reason=reason)
         # So the embed does not show 'user' as a class representation.
-        if user.__class__ not in {discord.Member, discord.User}:
-            user = _user
+        if not isinstance(user, (discord.Member, discord.User)):
+            user = puser
         await ctx.send(embed=self.client.templates.base_embed(f'Goodbye, {user}!'))
 
 
